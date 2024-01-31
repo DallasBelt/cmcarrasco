064c307b5597dec17574921646e9be33640bda3c
DallasBelt <31676836+DallasBelt@users.noreply.github.com>
2024-01-31 08:24:29 -0500

    -Add Express Validator Node package to validate and sanitize requests. -Add 'getPatientsByID' funtion. -Update 'updatePatient' funtion.

--- backend/controllers/patientsController.js
+++ backend/controllers/patientsController.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const patientsService = require('../services/patientsService')
 
 async function listPatients(req, res) {
@@ -11,6 +12,48 @@ async function listPatients(req, res) {
     return res.status(500).json({ statusCodeValue: 500, error: error.message })
   }
 }
+
+async function getPatientByID(req, res) {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    console.log('Errores de validación:', errors.array())
+    return res.status(400).json({ errors: errors.array() })
+  }
+
+  try {
+    const { cedula: patientID } = req.body
+
+    const patientData = await patientsService.getPatientByID(patientID)
+
+    if (patientData) {
+      return res.status(200).json(patientData);
+    } else {
+      return res.status(404).json({ message: 'Paciente no encontrado' })
+    }
+  } catch (error) {
+    console.error('Error al obtener datos del paciente por ID:', error)
+    return res.status(500).json({ message: 'Error interno del servidor' })
+  }
+}
+
+async function updatePatient(req, res) {
+  try {
+    console.log('updatePatient called')
+      const patientData = req.body;
+      console.log(patientData)
+      const updatedPatient = await patientsService.updatePatient(patientData);
+
+      if (updatedPatient) {
+          res.status(200).json({ message: 'Paciente actualizado con éxito', patient: updatedPatient });
+      } else {
+          res.status(404).json({ message: 'Paciente no encontrado' });
+      }
+  } catch (error) {
+      console.error('Error al actualizar el paciente:', error);
+      res.status(500).json({ error: 'Error interno del servidor' });
+  }
+}
+
 
 async function savePatient(req, res) {
   try {
@@ -29,30 +72,9 @@ async function savePatient(req, res) {
   }
 }
 
-async function actualizarPaciente(req, res) {
-  try {
-    // Obtiene los datos del paciente del cuerpo de la solicitud
-    const datosPaciente = req.body;
-
-    // Llama al servicio para guardar el paciente en la base de datos
-    const resultado = await patientsService.actualizarPaciente(datosPaciente);
-
-    // Verifica si se produjo un error al guardar el paciente
-    if (resultado.error) {
-      return res.status(resultado.status).json(resultado.message);
-    }
-
-    // Si se guardó correctamente, devuelve una respuesta exitosa
-    return res.status(200).json('Paciente actualizado correctamente.');
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ statusCodeValue: 500, error: error.message });
-  }
-}
-
 async function verifyID(req, res) {
   try {
-    const { patientID } = req.body
+    const { cedula: patientID } = req.body
     const isRegistered = await patientsService.verifyID(patientID)
     res.status(200).json({ registeredID: isRegistered, message: isRegistered ? 'La identificación ya está registrada.' : 'La identificación no está registrada.' })
   } catch (error) {
@@ -63,7 +85,7 @@ async function verifyID(req, res) {
 
 async function verifyEmail(req, res) {
   try {
-    const { patientEmail } = req.body
+    const { email: patientEmail } = req.body
     const isRegistered = await patientsService.verifyEmail(patientEmail)
     res.status(200).json({ registeredEmail: isRegistered, message: isRegistered ? 'El correo ya está registrado.' : 'El correo no está registrado.' })
   } catch (error) {
@@ -74,8 +96,9 @@ async function verifyEmail(req, res) {
 
 module.exports = {
   listPatients,
+  getPatientByID,
+  updatePatient,
   savePatient,
-  actualizarPaciente,
   verifyID,
   verifyEmail
 };
