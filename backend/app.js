f581627b1da411421b31f91bd0ddbc14b5e58e3f
DallasBelt <31676836+DallasBelt@users.noreply.github.com>
2024-01-31 08:12:58 -0500

    Change name to 'app.js' instead of 'index.js' to avoid confussion.

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+const loginRoute = require('./routes/loginRoute')
+const appointmentsRoute = require('./routes/appointmentsRoute')
+const medicsRoute = require('./routes/medicsRoute')
+const patientsRoute = require('./routes/patientsRoute')
+const recordsRoute = require('./routes/recordsRoute')
+const usersRoute = require('./routes/usersRoute')
+const { EncryptPassword } = require('./utils/encryptPassword')
+const mail = require('./utils/mailer')
+const citaService = require('./services/appointmentsService')
+const cron = require('node-cron')
+
+// middelwares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+
+//routes 
+app.use('/login', loginRoute)
+app.use('/appointments', appointmentsRoute)
+app.use('/medics', medicsRoute )
+app.use('/patients', patientsRoute)
+app.use('/records', recordsRoute)
+app.use('/users', usersRoute)
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+cron.schedule('30 * * * *', () => {
+  citaService.correoDeCitasPendientes()
+})
