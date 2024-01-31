14c6bedc336f73c1433f0175ed5eed97f2586db0
DallasBelt <31676836+DallasBelt@users.noreply.github.com>
2024-01-31 08:30:13 -0500

    Change SQL query.

--- backend/services/patientsService.js
+++ backend/services/patientsService.js
@@ -1,5 +1,5 @@
 const usuarioService = require('./usersService');
-const utilitarios = require('../utils/genUsername');
+const genUsername = require('../utils/genUsername');
 const pacienteQuerys = require('../queries/pacienteQuerys');
 const mail = require('../utils/mailer');
 const db = require('../db')
@@ -7,15 +7,137 @@ const db = require('../db')
 async function listPatients() {
   try {
     const patients = await db.any(`
-      SELECT * FROM paciente
+      SELECT *, id_usuario FROM paciente
     `)
 
     console.log(patients);
 
     return patients;
   } catch (error) {
-    console.error('Error al listar los pacientes:', error);
-    throw error; 
+    console.error('Error al listar los pacientes:', error)
+    throw error;
+  }
+}
+
+async function getPatientByID(patientID) {
+  const maritalStatusMapping = {
+    'Soltero(a)': 1,
+    'Casado(a)': 2,
+    'Divorciado(a)': 3,
+    'Viudo(a)': 4
+  }
+  try {
+    const query = `
+      SELECT *
+      FROM paciente
+      WHERE cedula = $1
+    `;
+
+    const patientData = await db.oneOrNone(query, [patientID]);
+
+    if (patientData) {
+      const mappedMaritalStatus = maritalStatusMapping[patientData.estado_civil];
+      if (mappedMaritalStatus) {
+        patientData.estado_civil = mappedMaritalStatus;
+      }
+      return patientData;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error('Error al obtener datos del paciente por ID:', error);
+    throw error;
+  }
+}
+
+// async function updatePatient(patientData) {
+//   try {
+//       const updateQuery = `
+//         UPDATE paciente 
+//         SET primer_nombre = $1,
+//             segundo_nombre = $2,
+//             primer_apellido = $3,
+//             segundo_apellido = $4,
+//             correo = $5,
+//             telefono_movil = $6,
+//             fecha_nacimiento = $7,
+//             estado_civil = $8,
+//             direccion = $9
+//         WHERE cedula = $10
+//         RETURNING *;`
+
+//       const values = [
+//         patientData.primer_nombre,
+//         patientData.segundo_nombre,
+//         patientData.primer_apellido,
+//         patientData.segundo_apellido,
+//         patientData.correo,
+//         patientData.telefono_movil,
+//         patientData.fecha_nacimiento,
+//         patientData.estado_civil,
+//         patientData.direccion,
+//         patientData.cedula
+//       ]
+
+//       const updatedPatient = await db.oneOrNone(updateQuery, values);
+
+//       return updatedPatient;
+//   } catch (error) {
+//       console.error('Error en el servicio al actualizar el paciente:', error);
+//       throw error;
+//   }
+// }
+
+async function updatePatient(patientData) {
+  const setClauses = [];
+  const values = [];
+
+  Object.entries(patientData).forEach(([key, value], index) => {
+      if (key !== 'cedula' && key !== 'id_usuario' && value !== undefined) {
+          setClauses.push(`${key} = $${index + 1}`);
+          values.push(value);
+      }
+  });
+
+  if (setClauses.length === 0) {
+      // No hay datos para actualizar
+      return null;
+  }
+
+  const updateQuery = `
+      UPDATE paciente
+      SET ${setClauses.join(', ')}
+      WHERE cedula = $${setClauses.length + 1}
+      RETURNING *;`;
+
+  values.push(patientData.cedula);
+
+  try {
+      const updatedPatient = await db.oneOrNone(updateQuery, values);
+      return updatedPatient;
+  } catch (error) {
+      console.error('Error al actualizar el paciente:', error);
+      throw error;
+  }
+}
+
+async function verifyID(patientID) {
+  try {
+      const result = await db.oneOrNone('SELECT * FROM paciente WHERE cedula = $1', [patientID]);
+      return !!result; // Devuelve true si se encuentra un paciente, false en caso contrario
+  } catch (error) {
+      console.error('Error al verificar el ID del paciente:', error);
+      throw error; // Propaga el error para manejarlo en el controlador
+  }
+}
+
+async function verifyEmail(patientEmail) {
+  try {
+      const result = await db.oneOrNone('SELECT * FROM paciente WHERE correo = $1', [patientEmail]);
+      return !!result; // Devuelve true si se encuentra un paciente, false en caso contrario
+  } catch (error) {
+      console.error('Error al verificar el correo del paciente:', error);
+      throw error; // Propaga el error para manejarlo en el controlador
   }
 }
 
@@ -23,7 +145,7 @@ async function listPatients() {
 async function savePatient(patient) {
   try {
     // Generar un nombre de usuario
-    const generatedUsername = utilitarios.nombreUsuario(patient.primer_nombre, patient.cedula);
+    const generatedUsername = genUsername.genUsername(patient.primer_nombre, patient.cedula);
 
     // Crear un nuevo usuario
     const newUser = {
@@ -75,56 +197,11 @@ async function savePatient(patient) {
   }
 }
 
-async function actualizarPaciente(paciente) {
-  try {
-    const nuevoPaciente = {
-      primer_nombre: paciente.primer_nombre,
-      segundo_nombre: paciente.segundo_nombre,
-      primer_apellido: paciente.primer_apellido,
-      segundo_apellido: paciente.segundo_apellido,
-      correo: paciente.correo,
-      telefono_movil: paciente.telefono_movil,
-      telefono_fijo: paciente.telefono_fijo,
-      fecha_nacimiento: paciente.fecha_nacimiento,
-      estado_civil: paciente.estado_civil,
-      cedula: paciente.cedula,
-      direccion: paciente.direccion
-    };
-
-    console.log(nuevoPaciente);
-    await pacienteQuerys.actualizarPaciente(nuevoPaciente);
-
-
-    return 'Paciente actualizado correctamente.';
-  } catch (error) {
-    throw new Error('Error al guardar el paciente en la base de datos');
-  }
-}
-
-async function verifyID(patientID) {
-  try {
-    const patient = await pacienteQuerys.verifyID(patientID)
-    return !!patient;
-  } catch (error) {
-    console.error('Error al verificar la identificación del paciente en el servicio:', error);
-    return false;
-  }
-}
-
-async function verifyEmail(patientEmail) {
-  try {
-    const patient = await pacienteQuerys.verifyEmail(patientEmail)
-    return !!patient;
-  } catch (error) {
-    console.error('Error al verificar el correo del paciente en el servicio:', error);
-    return false;
-  }
-}
-
 module.exports = {
   listPatients,
+  getPatientByID,
+  updatePatient,
   savePatient,
-  actualizarPaciente,
   verifyID,
   verifyEmail
-};
+}
