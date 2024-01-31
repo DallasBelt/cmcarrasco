5807ebdb332216c5d5681c723dc2f47cbafb7d83
DallasBelt <31676836+DallasBelt@users.noreply.github.com>
2024-01-31 08:34:38 -0500

    -Better organize variable declarations. -Change routes' mames to English.

--- backend/routes/patientsRoute.js
+++ backend/routes/patientsRoute.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const router = express.Router()
 const patientsController = require('../controllers/patientsController')
 
-const router = express.Router()
-
 router.get('/list', patientsController.listPatients)
+router.post('/get', patientsController.getPatientByID)
 router.post('/save', patientsController.savePatient)
-router.post('/actualizar', patientsController.actualizarPaciente)
+router.put('/update', patientsController.updatePatient)
 router.post('/verifyID', patientsController.verifyID)
 router.post('/verifyEmail', patientsController.verifyEmail)
 
