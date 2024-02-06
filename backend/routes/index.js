6dda4647cc6aaca36f32b42d328672e03160008b
DallasBelt <31676836+DallasBelt@users.noreply.github.com>
2024-02-05 22:18:05 -0500

    Delete no longer needed files.

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { obtenerUsuario } = require('../controllers/indexController');
-
-router.get('/users', obtenerUsuario);
-
-module.exports = router;
--- backend/utils/genUsername.js
+++
@@ -1,5 +0,0 @@
-const genUsername = (primerNombre, cedula) => primerNombre + cedula.substring(6)
-
-module.exports = {
-  genUsername
-}
